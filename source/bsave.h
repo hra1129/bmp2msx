3a349c3bb1c9295e3e5828c2c34dfcb3cd214cbe
HRA! <hra@mvj.biglobe.ne.jp>
2022-05-04 22:16:29 +0900

    Updated

--- source/bsave.h
+++ source/bsave.h
@@ -21,17 +21,17 @@ typedef struct {
 
 // パレットテーブルアドレス
 static const int PalTblAdr[]={
-	0x01B80L,		// screen 2/4
-	0x02020L,		// screen 3
-	0x07680L,		// screen 5
-	0x07680L,		// screen 6
-	0x0fA80L,		// screen 7
+	0x01B80,		// screen 2/4
+	0x02020,		// screen 3
+	0x07680,		// screen 5
+	0x07680,		// screen 6
+	0x0fA80,		// screen 7
 	0,				// screen 8
-	0,				// screen 10/11
+	0x0fA80,		// screen 10/11
 	0,				// screen 12
-	0x07680L,		// screen 5 (256 lines)
-	0x07680L,		// screen 6 (256 lines)
-	0x0fA80L,		// screen 7 (256 lines)
+	0x07680,		// screen 5 (256 lines)
+	0x07680,		// screen 6 (256 lines)
+	0x0fA80,		// screen 7 (256 lines)
 	0,				// screen 8 (256 lines)
 	0,				// screen 10/11 (256 lines)
 	0,				// screen 12 (256 lines)
